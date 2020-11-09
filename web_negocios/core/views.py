9cb84daf235596623e42cb89f4a803534d603fcc
patricia fuenzalida <paty.fum@gmail.com>
2020-11-09 19:15:55 -0300

    Agregar base de la navegacion
    Agregar urls de la app core
    Generar los templates de la app core
    Vincular los ficheros estaticos de la app core
    Enviar parametros desde view a template
    Integrar bootstrap y font awesome

--- web_negocios/core/views.py
+++ web_negocios/core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Encuentra tu negocio</h1>")
+    productos = []
+    for i in range(1,10):
+        prod = {"nombre": "producto " + str(i), "precio": i * 1000}
+        productos.append(prod)
+    return render(request,"index.html", {'titulo': 'Ofrece aquí tus productos', 'productos':productos})
 
 def contacto(request):
     return HttpResponse("<h1>Esto es contacto</h1>")
